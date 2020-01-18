5d8b1f522ce9db3500eecd58580995b16f06a239
mesako <mesako@stanford.edu>
2020-01-17 22:34:26 -0800

    add workshop R file

---
+++ SABERWest2020_Workshop.R
@@ -0,0 +1,96 @@
+
+###################################################################
+###### Reproducible Analysis in R Workshop at SABERWest 2020 ######
+###################################################################
+
+### LEARNING GOALS:
+# -	Describe how Git can be used to store and document different
+#   versions of a set of files
+# -	Build and document a package in R for easy installation
+# -	Demonstrate how users can collaborate on the same project using Github
+# -	Select and apply the most appropriate tool or set of tools for
+#   reproducible data analysis
+# -	Integrate useful software tools into an existing analysis pipeline
+
+
+### AGENDA
+
+### Research Collaboration/Replication ###
+
+# Think of one time when collaboration in a project or
+# replication of an existing study was made easier for you and
+# one time when it was made more difficult.
+
+# Share your thoughts with a neighbor.
+
+
+### How We Share Code ###
+
+# In groups, discuss your current system for working with, storing,
+# and sharing code that you use for research.
+
+# What are the pros and cons of your systems?
+
+# Outline what improvements you would want to make and how this
+# workshop could meet those needs.
+
+
+### Why Versioning? ###
+
+### Make a Git Repo
+
+### Make and Save Changes
+
+### Revert to Old Versions
+
+
+
+### What is a Software Package? ###
+
+# In groups of 3-4, discuss what a software package is and contrast with
+# the current workspace. What are the components of a software package?
+
+# What distinguishes a package/library from a set of files like you just created?
+
+# Why would we use packages?
+
+
+### Making an R Package ###
+
+# NAMESPACE
+
+
+
+### User-Friendly Code ###
+
+# If you shared this code with someone else in your research group,
+# or another research collaborator in the field, what would they want
+# to know to use your code?
+
+
+### Code Documentation
+
+# Functions
+
+
+
+### Using GitHub ###
+
+# Individually reflect how you can share files over the internet.
+
+# What are the pros and cons of each method? 
+
+
+### Pushing Code to GitHub ###
+
+
+
+### Action Plan ###
+
+# Reflect individually on your top three action items for when you revisit
+# your data analysis set-up.
+
+# What might be a few practical and realistic changes you can make to
+# make things easier or more convenient for you in your research?
+  
+  
